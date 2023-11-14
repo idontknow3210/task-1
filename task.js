3e8db5422e6e7813dd4bf93508523cac205d7848
Yura <bobr515yura@yandex.ru>
2023-11-14 13:45:53 +0300

    TASK1

--- task.js
+++ task.js
@@ -4,14 +4,14 @@ function solveEquation(a, b, c) {
     let d = Math.pow(b, 2) - 4 * a * c;
     let result;
     if (d<0) {
-        result = ` `;
+        result = [];
     } else if (d===0) {
-        result = (-b / (2 * a)) ;
+        result = [(-b / (2 * a))] ;
     } else {
-        result = ((-b + Math.sqrt(d) )/(2*a)) + ", " + ((-b - Math.sqrt(d) )/(2*a));
+        result = [((-b + Math.sqrt(d) )/(2*a)), ((-b - Math.sqrt(d) )/(2*a))];
 
     }
-    return `[${result}]`;
+    return result;
     
 
 }
